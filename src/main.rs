b549f98f26b570fc7aad417df5ed86a7c9a9a9c2
VentGrey <omarpurataf@gmail.com>
2018-10-01 21:46:11 -0500

    Simulando una petición lenta cada 5 segundos, no debería explotar nada

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,10 @@ use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
 
+//Multihilo (Esta cosa es del diablo, no la toque si no sabe lo que hace)
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let listen = TcpListener::bind("127.0.0.1:7878").unwrap();
 
@@ -23,17 +27,23 @@ fn main() {
     }
 }
 
+
+
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hola.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK\r\n\r\n", "hola.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND!\r\n\r\n", "404.html")
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
 
         let contents = fs::read_to_string(filename).unwrap();
