ba31f23e8a370456f7b9f4ef8661b25f92c1050c
VentGrey <omarpurataf@gmail.com>
2018-10-01 21:11:46 -0500

    se agregó la biblioteca estándar para leer del sistema de archivos

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+use std::fs;
 
 fn main() {
     let escucha = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -26,6 +27,8 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("hola.html").unwrap();
 
     let respuesta = "HTTP/1.1 200 OK\r\n\r\n";
 
