51496718d30d3eb132c5de32f769174c81a4d52d
VentGrey <omarpurataf@gmail.com>
2018-10-01 21:35:30 -0500

    Se arreglaron redundancias de código

--- src/main.rs
+++ src/main.rs
@@ -30,21 +30,16 @@ fn handle_connection(mut stream: TcpStream) {
 
     let get = b"GET / HTTP/1.1\r\n";
 
-    if buffer.starts_with(get) {
-
-        let contents = fs::read_to_string("hola.html").unwrap();
-
-        let response = "HTTP/1.1 200 OK\r\n\r\n";
-
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "hola.html")
     } else {
-        let status_line = "HTTP/1.1 404 NO ENCONTRADO\r\n\r\n";
-        let contents = fs::read_to_string("404.html").unwrap();
+        ("HTTP/1.1 404 NO ENCONTRADO\r\n\r\n", "404.html")
+    };
+
+        let contents = fs::read_to_string(filename).unwrap();
 
         let response = format!("{}{}", status_line, contents);
 
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-    }
 }
