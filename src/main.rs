eacf15215324752ecb7b74c657330a93cd38f7b5
VentGrey <omarpurataf@gmail.com>
2018-10-01 19:12:43 -0500

    prueba de conexión simple usando la biblioteca standard con net

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let escucha = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in escucha.incoming() {
+        let stream = stream.unwrap();
+
+        println!("¡Conexión establecida!");
+    }
 }
