370bbb6804c77f1bd1b5f95772d6aa9207b19c6e
VentGrey <omarpurataf@gmail.com>
2018-10-01 19:23:21 -0500

    Mejor lo explicamos en la tarea XD

--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,8 @@
  * Autor: Omar Jair Purata Funes
  * ----------EOF-----------*/
 
-/* Para empezar nuestro servidor web necesitará "escuchar" una conexión TCP
- * Nosotros vamos a trabajar en dicha parte, pues la biblioteca std de Rust
- * ofrece un módulo std::net que nos permite hacer ésto.*/
-
 use std::net::TcpListener;
 
-
-/* La función main escuchará en la dirección 127.0.0.1:7878 por algún stream
- * de TCP. Cuando encuentre uno ésta enviará a pantalla el mensaje
- * "Conexión establecida" */
 fn main() {
     let escucha = TcpListener::bind("127.0.0.1:7878").unwrap();
 
